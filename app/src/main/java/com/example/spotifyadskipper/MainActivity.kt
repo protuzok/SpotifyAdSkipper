c0c663020445c9318a5cf387502b721d4a2fa489
turyk <turykruslanz@gmail.com>
2026-01-13 14:50:02 +0200

    delete buttons for stopping script and for going to spotify

--- app/src/main/java/com/example/spotifyadskipper/MainActivity.kt
+++ app/src/main/java/com/example/spotifyadskipper/MainActivity.kt
@@ -1,27 +1,16 @@
 package com.example.spotifyadskipper
 
-import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn = findViewById<Button>(R.id.btnToggle)
         val btnAcc = findViewById<Button>(R.id.btnAccessibility)
-        val btnSpotify = findViewById<Button>(R.id.btnOpenSpotify)
         val btnPocket = findViewById<Button>(R.id.btnPocketMode)
-
-        updateButtonUI(btn)
-
-        btn.setOnClickListener {
-            AppConfig.isEnabled = !AppConfig.isEnabled
-            updateButtonUI(btn)
-        }
 
         // Логіка для переходу в налаштування
         btnAcc.setOnClickListener {
@@ -29,35 +18,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        btnSpotify.setOnClickListener {
-            val packageName = "com.spotify.music" // Стандартне ім'я пакета Spotify
-            val intent = packageManager.getLaunchIntentForPackage(packageName)
-
-            if (intent != null) {
-                startActivity(intent)
-            } else {
-                // Якщо додаток не встановлено, можна відкрити його в Play Store
-                val marketIntent = android.content.Intent(
-                    android.content.Intent.ACTION_VIEW,
-                    "market://details?id=$packageName".toUri()
-                )
-                startActivity(marketIntent)
-            }
-        }
-
         btnPocket.setOnClickListener {
             val intent = android.content.Intent(this, PocketModeActivity::class.java)
             startActivity(intent)
-        }
-    }
-
-    private fun updateButtonUI(btn: Button) {
-        if (AppConfig.isEnabled) {
-            btn.text = "СКРИПТ ПРАЦЮЄ !!! (НАТИСНИ ЩОБ ЗУПИНИТИ)"
-            btn.setBackgroundColor(Color.RED)
-        } else {
-            btn.text = "СКРИПТ ВИМКНЕНО (НАТИСНИ ЩОБ ЗАПУСТИТИ)"
-            btn.setBackgroundColor(Color.GREEN)
         }
     }
 }
